365856a5550fca89f28070a6f99f27f8e11c265b
ashpil <shpilenok2001@gmail.com>
2021-03-05 22:20:30 -0500

    starter with svelte+routify

---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style global>
+  @import 'assets/styles/global.scss';
+  @import 'assets/styles/typography.scss';
+</style>
+
+<Router {routes} />
+
---
+++ frontend/src/pages/index.svelte
@@ -0,0 +1,38 @@
+<script>
+    import mapboxgl from 'mapbox-gl';
+    import { onMount } from 'svelte';
+
+    let mapContainer;
+    
+    onMount(async () => {
+      const map = new mapboxgl.Map({
+        container: mapContainer,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        accessToken: 'pk.eyJ1IjoiYWlzNzYiLCJhIjoiY2tseDNiMHM4MDB5eTJ2cnM0YnZsdTQ0cSJ9.o3Bh8gznViclDtI-zg6t5g'
+      });
+	});
+</script>
+
+<style>
+  #map {
+    height: 100vh;
+  }
+  main {
+    z-index: 1;
+    position: relative;
+  }
+  h1 {
+    margin: 1rem;
+    background-color: white;
+    position: absolute;
+    border-radius: 1rem;
+    padding: 0.25em 0.5em;
+    box-shadow: 0px 0px 14px 1px rgba(0,0,0,0.2);
+  }
+</style>
+
+<main>
+  <h1>Hello. Please run</h1>
+</main>
+<div id="map" bind:this={mapContainer}></div>
+
